83a9b37f19d3f79db962c71ea8dba873a22d48a1
BruceLYX <homo.sapiens851105@gmail.com>
2021-12-29 23:17:17 +0800

    data to colab and simple baseline

--- model/cnn.py
+++ model/cnn.py
@@ -55,3 +55,49 @@ class CNN(nn.Module):
 
         return feature
 
+
+class MyCnnNet(nn.Module):
+
+    def __init__(self, num_classes=10):
+        super(MyCnnNet, self).__init__()
+        
+     
+        self.conv1 = nn.Conv2d(3, 6, kernel_size=5, stride=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(6)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5, stride=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(16)  
+        
+        self.relu = nn.ReLU(inplace=True)
+        self.pool = nn.MaxPool2d(2,2)
+        
+        self.fc1 = nn.Linear(16*4*4,120)
+        self.fc2 = nn.Linear(120,84)
+        self.fc3 = nn.Linear(84, num_classes)
+        
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.pool(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        
+        x = x.view(-1, 16*4*4)
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
+        
+
+        return x
