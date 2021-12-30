6565c03bc6516505741f354561569ecae424ca2d
BruceLYX <homo.sapiens851105@gmail.com>
2021-12-30 23:16:07 +0800

    close to strong and more model

--- model/cnn.py
+++ model/cnn.py
@@ -56,7 +56,7 @@ class CNN(nn.Module):
         return feature
 
 
-class MyCnnNet(nn.Module):
+class MyCnnNet(nn.Module):  # my first try
 
     def __init__(self, num_classes=10):
         super(MyCnnNet, self).__init__()
@@ -95,6 +95,168 @@ class MyCnnNet(nn.Module):
         x = self.pool(x)
         
         x = x.view(-1, 16*4*4)
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
+        
+
+        return x
+        
+
+class PhDCnn(nn.Module):  # youutbe
+
+    def __init__(self, num_classes=10):
+        super(PhDCnn, self).__init__()
+        
+     
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(64)  
+        
+        self.relu = nn.ReLU(inplace=True)
+        self.pool = nn.MaxPool2d(2,2)
+        
+        self.fc1 = nn.Linear(64*4*4,120)
+        self.fc2 = nn.Linear(120,84)
+        self.fc3 = nn.Linear(84, num_classes)
+        
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.pool(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+
+        
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        
+        x = x.view(-1, 64*4*4)
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
+        
+
+        return x
+        
+
+class MyCnnNet(nn.Module):  # my first try
+
+    def __init__(self, num_classes=10):
+        super(MyCnnNet, self).__init__()
+        
+     
+        self.conv1 = nn.Conv2d(3, 6, kernel_size=5, stride=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(6)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5, stride=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(16)  
+        
+        self.relu = nn.ReLU(inplace=True)
+        self.pool = nn.MaxPool2d(2,2)
+        
+        self.fc1 = nn.Linear(16*4*4,120)
+        self.fc2 = nn.Linear(120,84)
+        self.fc3 = nn.Linear(84, num_classes)
+        
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.pool(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        
+        x = x.view(-1, 16*4*4)
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
+        
+
+        return x
+        
+
+class PhDCnn_deep(nn.Module):  # youutbe
+
+    def __init__(self, num_classes=10):
+        super(PhDCnn_deep, self).__init__()
+        
+     
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(64)  
+        self.conv4 = nn.Conv2d(64, 64, kernel_size=3, stride=1, bias=False)
+        self.bn4 = nn.BatchNorm2d(64) 
+        
+        self.relu = nn.ReLU(inplace=True)
+        self.pool = nn.MaxPool2d(2,2)
+        
+        self.fc1 = nn.Linear(64*4*4,120)
+        self.fc2 = nn.Linear(120,84)
+        self.fc3 = nn.Linear(84, num_classes)
+        
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+
+    def forward(self, x):
+   
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+    
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.pool(x)
+
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu(x)
+
+        
+        x = self.conv4(x)
+        x = self.bn4(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        
+        x = x.view(-1, 64*4*4)
         x = self.relu(self.fc1(x))
         x = self.relu(self.fc2(x))
         x = self.fc3(x)
